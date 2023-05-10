06bfce3030350ddbec33329e3c0c23640d5d9608
ash.blade <ashen.blade.dev@gmail.com>
2023-05-10 14:59:42 +0300

    Добавил проект с моделями

---
+++ backend/Mqtt.Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Mqtt.Models;
+
+/// <summary>
+/// Пуш, передаваемый на андроид-клиент
+/// </summary>
+public class Message
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string Template { get; set; }
+}
